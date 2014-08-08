e8e3f11e81dee0baa8f5889da51a3c091e1ca589
ttacon <ttacon@gmail.com>
2014-08-08 15:54:06 -0400

    Folder struct

--- box.go
+++ box.go
@@ -2,9 +2,10 @@ package box
 
 import (
 	"bytes"
-	"code.google.com/p/goauth2/oauth"
 	"encoding/json"
 	"fmt"
+
+	"code.google.com/p/goauth2/oauth"
 )
 
 const (
@@ -16,7 +17,89 @@ type Client struct {
 	Trans *oauth.Transport
 }
 
-func (c *Client) CreateFolder(name string, parent int) error {
+// TODO(ttacon): go through and clean up pointer vs non-pointer
+// TODO(ttacon): go through and see where omitempty is appropriate
+type AccessEmail struct {
+	// TODO(ttacon): these may change...
+	Access string `json:"access,omitempty"`
+	Email  string `json:"email,omitempty"`
+}
+
+type Order struct {
+	By        string `json:"by"`
+	Direction string `json:"direction"`
+}
+
+type ItemCollection struct {
+	TotalCount int      `json:"total_count,omitempty"`
+	Entries    []*Item  `json:"entries,omitempty"` // this is probably items... TODO(ttacon): double check
+	Offset     int      `json:"offset,omitempty"`
+	Limit      int      `json:"limit,omitempty"`
+	Order      []*Order `json:"order"`
+}
+
+type PathCollection struct {
+	TotalCount int     `json:"total_count"`
+	Entries    []*Item `json:"entries"`
+}
+
+type Item struct {
+	Type       string  `json:"type,omitempty"` // TODO(ttacon): make this an enum eventually
+	ID         string  `json:"id,omitempty"`
+	SequenceId string  `json:"sequence_id,omitempty"` // no idea what this is supposed to be
+	ETag       *string `json:"etag,omitempty"`        // again, not sure what this type is supposed to be
+	Name       string  `json:"name,omitempty"`
+	Login      string  `json:"login,omitempty"`
+}
+
+// TODO(ttacon): leave plurality?
+type Permissions struct {
+	CanDownload bool `json:"can_download"`
+	CanPreview  bool `json:"can_preview"`
+}
+
+type Link struct {
+	Url               string       `json:"url"`
+	DownloadUrl       *string      `json:"download_url"`
+	VanityUrl         *string      `json:"vanity_url"`
+	IsPasswordEnabled bool         `json:"is_password_enabled"`
+	UnsharedAt        *string      `json:"unshared_at"` // TODO(ttacon): change to time.Time
+	DownloadCount     int          `json:"download_count"`
+	PreviewCount      int          `json:"preview_count"`
+	Access            string       `json:"access"` // TODO(ttacon): consider enums for these types of values?
+	Permissions       *Permissions `json:"permissions"`
+}
+
+type Folder struct {
+	ID                string          `json:"id,omitempty"`
+	FolderUploadEmail *AccessEmail    `json:"folder_upload_email,omitempty"`
+	Parent            *Item           `json:"parent,omitempty"`
+	ItemStatus        string          `json:"item_status"`
+	ItemCollection    *ItemCollection `json:"item_collection"`
+	Type              string          `json:"type"`
+	Description       string          `json:"description"`
+	Size              int             `json:"size"`
+	CreateBy          *Item           `json:"created_by"`
+	ModifiedBy        *Item           `json:"modified_by"`
+	TrashedAt         *string         `json:"trashed_at"`          // TODO(ttacon): change to time.Time
+	ContentModifiedAt *string         `json:"content_modified_at"` // TODO(ttacon): change to time.Time
+	PurgedAt          *string         `json:"purged_at"`           // TODO(ttacon): change to time.Time, this field isn't documented but I keep getting it back...
+	SharedLinkg       *string         `json:"shared_link"`
+	SequenceId        string          `json:"sequence_id"`
+	ETag              *string         `json:"etag"`
+	Name              string          `json:"name"`
+	CreatedAt         *string         `json:"created_at"` // TODO(ttacon): change to time.Time
+	OwnedBy           *Item           `json:"owned_by"`
+	ModifiedAt        *string         `json:"modified_at"`        // TODO(ttacon): change to time.Time
+	ContentCreatedAt  *string         `json:"content_created_at"` // TODO(ttacon): change to time.Time
+	PathCollection    *PathCollection `json:"path_collection"`    // TODO(ttacon): make sure this is the correct kind of struct(ure)
+	SharedLink        *Link           `json:"shared_link"`
+}
+
+// TODO(ttacon): return the response so the user can check the status code
+// or we should check it? it's more flexible if we let the user decide what
+// they view as an error
+func (c *Client) CreateFolder(name string, parent int) (*Folder, error) {
 	var body = map[string]interface{}{
 		"name": name,
 		"parent": map[string]int{
@@ -27,7 +110,7 @@ func (c *Client) CreateFolder(name string, parent int) error {
 	buf, err := json.Marshal(body)
 	if err != nil {
 		fmt.Println("err: ", err)
-		return err
+		return nil, err
 	}
 
 	resp, err := c.Trans.Client().Post(
@@ -36,15 +119,15 @@ func (c *Client) CreateFolder(name string, parent int) error {
 		bytes.NewReader(buf))
 	if err != nil {
 		fmt.Println("err: ", err)
-		return err
+		return nil, err
 	}
 
-	var data = make(map[string]interface{})
+	var data Folder
+	fmt.Println("resp: ", resp)
 	err = json.NewDecoder(resp.Body).Decode(&data)
 	if err != nil {
 		fmt.Println("err: ", err)
-		return err
+		return nil, err
 	}
-	fmt.Println(data)
-	return nil
+	return &data, nil
 }
