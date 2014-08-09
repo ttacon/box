e7ac86ecc7850049df5f4d360a605ca1d10565e7
ttacon <ttacon@gmail.com>
2014-08-09 13:09:17 -0400

    Add GetTrashedFolder example

---
+++ examples/folders/gettrashedfolder.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	"code.google.com/p/goauth2/oauth"
+	"github.com/kr/pretty"
+	"github.com/ttacon/box"
+)
+
+var (
+	clientId     = flag.String("cid", "", "OAuth Client ID")
+	clientSecret = flag.String("csec", "", "OAuth Client Secret")
+
+	accessToken  = flag.String("atok", "", "Access Token")
+	refreshToken = flag.String("rtok", "", "Refresh Token")
+)
+
+func main() {
+	flag.Parse()
+
+	// Set our OAuth2 configuration up
+	var (
+		config = &oauth.Config{
+			ClientId:     *clientId,
+			ClientSecret: *clientSecret,
+			Scope:        "",
+			AuthURL:      "https://www.box.com/api/oauth2/authorize",
+			TokenURL:     "https://www.box.com/api/oauth2/token",
+		}
+
+		tok = &oauth.Transport{
+			Config: config,
+			Token: &oauth.Token{
+				AccessToken:  *accessToken,
+				RefreshToken: *refreshToken,
+				Expiry:       time.Now(), // I do this as box expires tokens each hour
+			},
+		}
+	)
+
+	var c = &box.Client{tok}
+
+	resp, folder, err := c.GetTrashedFolder("2303056557")
+	fmt.Println("resp: ", resp)
+	fmt.Println("err: ", err)
+	pretty.Print(folder)
+
+	// Print out the new tokens for next time
+	fmt.Printf("%#v\n", tok.Token)
+}
