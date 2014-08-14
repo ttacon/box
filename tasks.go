f73b42f5f579b638aabf9d3cb741d3121bbe822e
ttacon <ttacon@gmail.com>
2014-08-14 12:45:21 -0400

    Add get task assignments

--- tasks.go
+++ tasks.go
@@ -154,3 +154,24 @@ func (c *Client) DeleteTask(taskId string) (*http.Response, error) {
 
 	return c.Trans.Client().Do(req)
 }
+
+// Documentation: https://developers.box.com/docs/#tasks-get-the-assignments-for-a-task
+func (c *Client) GetAssignmentsForTask(taskId string) (*http.Response, *TaskAssignmentCollection, error) {
+	req, err := http.NewRequest(
+		"GET",
+		fmt.Sprintf("%s/tasks/%s/assignments", BASE_URL, taskId),
+		nil,
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	resp, err := c.Trans.Client().Do(req)
+	if err != nil {
+		return resp, nil, err
+	}
+
+	var data TaskAssignmentCollection
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	return resp, &data, err
+}
