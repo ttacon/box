e36d446b3d419aea44c675802775bf126f96c767
ttacon <ttacon@gmail.com>
2014-08-14 08:10:06 -0400

    Add AddCollaboration

--- collaborations.go
+++ collaborations.go
@@ -1,7 +1,16 @@
 package box
 
-// TODO(ttacon):some of these fields pop up everywhere, make
-// common struct and anonymously extend the others with it?
+import (
+	"bytes"
+	"encoding/json"
+
+	// TODO(ttacon):some of these fields pop up everywhere, make
+	// common struct and anonymously extend the others with it?
+	// Documentation: https://developers.box.com/docs/#collaborations
+	"fmt"
+	"net/http"
+)
+
 type Collaboration struct {
 	Type           string  `json:"type"`
 	ID             string  `json:"id"`
@@ -19,4 +28,54 @@ type Collaboration struct {
 type Collaborations struct {
 	TotalCount int `json:"total_count"`
 	Entries    []*Collaboration
+}
+
+// Documentation: https://developers.box.com/docs/#collaborations-add-a-collaboration
+func (c *Client) AddCollaboration(
+	itemId,
+	itemType,
+	accessibleId,
+	accessibleType,
+	accessibleEmail,
+	role string) (*http.Response, *Collaboration, error) {
+	// TODO(ttacon): shrink param list
+
+	var dataMap = map[string]interface{}{
+		"item": map[string]string{
+			"id":   itemId,
+			"type": itemType,
+		},
+		"accessible_by": map[string]string{
+			"id":   accessibleId,
+			"type": accessibleType,
+		},
+		"role": role,
+	}
+	if len(accessibleEmail) > 0 {
+		v, _ := dataMap["accessible_by"].(map[string]string)
+		v["login"] = accessibleEmail
+	}
+
+	dataBytes, err := json.Marshal(dataMap)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := http.NewRequest(
+		"POST",
+		fmt.Sprintf("%s/collaborations", BASE_URL),
+		bytes.NewReader(dataBytes),
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	resp, err := c.Trans.Client().Do(req)
+	if err != nil {
+		return resp, nil, err
+	}
+
+	var data Collaboration
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	return resp, &data, err
 }
